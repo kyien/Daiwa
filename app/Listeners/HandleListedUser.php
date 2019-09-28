6db56c8f51402ef073a1f84d37e3374a5d6cedf8
addi_kyien <kyien.njoroge@hotmail.com>
2019-09-28 16:46:31 +0300

    prod v2

--- app/Listeners/HandleListedUser.php
+++ app/Listeners/HandleListedUser.php
@@ -6,6 +6,7 @@ use App\Events\UserListed;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use App\Libraries\Sms;
+use Auth;
 
 // use Auth;
 
@@ -32,13 +33,14 @@ class HandleListedUser
 
     public function handle(UserListed $event)
     {
+        $user=Auth::guard('tokenuser')->user();
         //
         $id=mt_rand(000000,999999);
         $debtor_phone=$event->debtor->Mobile_no;
         $credentials=array(
             'msisdn'=>$debtor_phone,
             'destination'=>'DAIWA_SASA',
-            'message'=>'hello'.$event->debtor->Fullnames.'You have been listed in Daiwa Sasa platform By'.auth()->user()->firstname.'using the No.'.auth()->user()->mobile_no,
+            'message'=>'hello'.$event->debtor->Fullnames.'You have been listed to the Daiwa Sasa platform By'.$user->firstname.'using the No.'.$user->mobile_no.'for the debt of kshs'.$event->debtor->Amount_owed,
             'sms_id'=>$id
           );
         $sms=new Sms;
